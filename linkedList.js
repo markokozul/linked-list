d09554a51500acc100cc0720f0272fce3e1bdb33
Marko <mkozul589@gmail.com>
2023-05-02 16:42:24 +0200

    added insertAt and removeAt functions

--- linkedList.js
+++ linkedList.js
@@ -6,25 +6,23 @@ const linkedList = () => {
   const append = (node) => {
     if (head === null) {
       head = node;
-      length++;
     } else {
       let last = lastNode();
       last.nextNode = node;
-      length++;
-      console.log(last.nextNode);
     }
+    length++;
   };
 
+  //adding to the start of the list
   const prepend = (node) => {
     if (head === null) {
       head = node;
-      length++;
     } else {
       let headPlaceholder = head;
       head = node;
       head.nextNode = headPlaceholder;
-      length++;
     }
+    length++;
   };
 
   const getSize = () => {
@@ -86,7 +84,14 @@ const linkedList = () => {
   };
   const pop = () => {
     let secondToLast = findByIndex(length - 2);
-    secondToLast.nextNode = null;
+    if (secondToLast) {
+      secondToLast.nextNode = null;
+    } else {
+      head = null;
+    }
+    length--;
+
+    return "success";
   };
 
   const toString = () => {
@@ -101,7 +106,59 @@ const linkedList = () => {
 
     return listString;
   };
+  const insertAt = (value, index) => {
+    if (head === null) {
+      return null;
+    }
+    if (findByIndex(index)) {
+      let node = createNode(value);
+      let nodeAtIndex = findByIndex(index);
+      let previousNode = findByIndex(index - 1);
+      let next = findByIndex(index + 1);
 
+      if (!previousNode && next) {
+        prepend(node);
+      }
+      if (!next && previousNode) {
+        append(node);
+      } else {
+        previousNode.nextNode = node;
+        node.nextNode = nodeAtIndex;
+      }
+      length++;
+      return "success";
+    } else {
+      console.log("No nodes on given index");
+      return null;
+    }
+  };
+  const removeAt = (index) => {
+    if (head === null) {
+      return null;
+    }
+    if (findByIndex(index)) {
+      let nodeToRemove = findByIndex(index);
+      let previousNode = findByIndex(index - 1);
+      let next = findByIndex(index + 1);
+
+      if (!previousNode && next) {
+        head = next;
+      } else if (!next && previousNode) {
+        pop();
+      } else {
+        previousNode.nextNode = next;
+        nodeToRemove.nextNode = null;
+      }
+      length--;
+
+      console.log(toString());
+      return "success";
+    } else {
+      console.log("No nodes on given index");
+
+      return null;
+    }
+  };
   return {
     append,
     prepend,
@@ -113,6 +170,8 @@ const linkedList = () => {
     contains,
     findByValue,
     toString,
+    insertAt,
+    removeAt,
   };
 };
 
@@ -124,22 +183,33 @@ const createNode = (newValue) => {
   return { value, nextNode };
 };
 
-let node1 = createNode(10);
-let node2 = createNode(5);
-let node3 = createNode(22);
-let node4 = createNode("lol");
+const testing = (() => {
+  let node1 = createNode(10);
+  let node2 = createNode(5);
+  let node3 = createNode(22);
+  let node4 = createNode("abcd");
 
-list.append(node1);
-list.append(node2);
-list.append(node3);
+  list.append(node1);
+  list.append(node2);
+  list.append(node3);
+  list.prepend(node4);
 
-list.prepend(node4);
-
-console.log(list.getSize());
-console.log(list.getHead());
-console.log(list.findByIndex(0));
-console.log(list.contains(67));
-console.log(list.findByValue(5));
-console.log(list.toString());
-
-list.pop();
+  console.log("Size of the list-> " + list.getSize());
+  console.log("Head node of the list:");
+  console.log(list.getHead());
+  console.log("Find node on index 1:");
+  console.log(list.findByIndex(1));
+  console.log("List contains value 67-> " + list.contains(67));
+  console.log("Find index of node containing value 5: ");
+  console.log(list.findByValue(5));
+  console.log("Insert node at index 2: ");
+  console.log(list.insertAt("qwerty", 2));
+  console.log("Remove node at index 6: ");
+  console.log(list.removeAt(6));
+  console.log("Remove from end of the list: ");
+  console.log(list.pop());
+  console.log("Size of the list");
+  console.log(list.getSize());
+  console.log("toString method: ");
+  console.log(list.toString());
+})();
